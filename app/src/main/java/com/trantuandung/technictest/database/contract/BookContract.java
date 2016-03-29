d34499b7c8a34b8cafa2373d7662af07a5b46804
TRAN Tuan Dung <t2dung8x@gmail.com>
2016-03-29 23:02:47 +0200

    feat(Database): add Book's model and Book's contract

--- app/src/main/java/com/trantuandung/technictest/database/contract/BookContract.java
+++ app/src/main/java/com/trantuandung/technictest/database/contract/BookContract.java
@@ -1,4 +1,42 @@
 package com.trantuandung.technictest.database.contract;
 
-public class BookContract {
+import android.content.ContentResolver;
+import android.provider.BaseColumns;
+
+import com.trantuandung.technictest.database.ProviderInfos;
+import com.trantuandung.technictest.database.enums.DBTable;
+
+public class BookContract implements BaseColumns {
+    public static final String COL_ISBN = "isbn";
+    public static final String COL_TITLE = "title";
+    public static final String COL_PRICE = "price";
+    public static final String COL_COVER = "cover";
+
+    public static final String SORT_ORDER_TITLE = "lower(" + COL_TITLE + ") ASC";
+    public static final String SORT_ORDER_PRICE = COL_PRICE + " ASC, " + "lower(" + COL_TITLE + ") ASC";
+
+    public static final String TABLE_NAME = DBTable.BOOK.getValue();
+
+    public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + ProviderInfos.AUTHORITY + "." + TABLE_NAME;
+
+    public static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ("
+            + _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COL_ISBN + " TEXT NOT NULL, "
+            + COL_TITLE + " TEXT NOT NULL, "
+            + COL_PRICE + " INTEGER DEFAULT 0, "
+            + COL_COVER + " TEXT DEFAULT NULL);"
+            + String.format("CREATE INDEX %s_index on %s (%s)", TABLE_NAME, TABLE_NAME, COL_ISBN) + ";";
+    //
+    private static final String[] projection = new String[]{
+            _ID,
+            COL_ISBN,
+            COL_TITLE,
+            COL_PRICE,
+            COL_COVER
+    };
+
+    public static String[] getProjection() {
+        return projection;
+    }
 }
--- app/src/main/java/com/trantuandung/technictest/database/model/Book.java
+++ app/src/main/java/com/trantuandung/technictest/database/model/Book.java
@@ -1,4 +1,60 @@
 package com.trantuandung.technictest.database.model;
 
+import android.content.ContentValues;
+import android.text.TextUtils;
+
+import com.trantuandung.technictest.database.contract.BookContract;
+
 public class Book {
+    private String isbn;
+    private String title;
+    private int price;
+    private String cover;
+
+    public String getCover() {
+        return cover;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public ContentValues getContentValues() {
+        ContentValues values = new ContentValues();
+        values.put(BookContract.COL_ISBN, isbn);
+        values.put(BookContract.COL_TITLE, title);
+        values.put(BookContract.COL_PRICE, price);
+        values.put(BookContract.COL_COVER, cover);
+        return values;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+
+        Book book = (Book) o;
+
+        return price == book.getPrice() &&
+                !TextUtils.isEmpty(isbn) && !isbn.equals(book.getIsbn()) &&
+                !TextUtils.isEmpty(title) && title.equals(book.getTitle()) &&
+                !TextUtils.isEmpty(cover) && cover.equals(book.getCover());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getIsbn() != null ? getIsbn().hashCode() : 0;
+        result = 31 * result + (getTitle() != null ? getTitle().hashCode() : 0);
+        result = 31 * result + getPrice();
+        result = 31 * result + (getCover() != null ? getCover().hashCode() : 0);
+        return result;
+    }
 }
