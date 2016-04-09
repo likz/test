3543b6b9e324b0e6bdb96cc23ed805329fa3a0f3
TRAN Tuan Dung <t2dung8x@gmail.com>
2016-04-09 13:05:34 +0200

    feat(ItemsRequester): integrate new method getAllOfferByBooks

--- app/src/main/java/com/trantuandung/technictest/controller/server/ItemsRequester.java
+++ app/src/main/java/com/trantuandung/technictest/controller/server/ItemsRequester.java
@@ -7,6 +7,9 @@ import com.google.gson.reflect.TypeToken;
 import com.trantuandung.technictest.controller.server.configure.ServerConfiguration;
 import com.trantuandung.technictest.controller.server.connected.ServerInterface;
 import com.trantuandung.technictest.database.model.Book;
+import com.trantuandung.technictest.database.model.Offer;
+
+import org.json.JSONObject;
 
 import java.lang.reflect.Type;
 import java.util.List;
@@ -28,6 +31,23 @@ public class ItemsRequester {
             return gson.fromJson(jsonBody, collectionType);
         } catch (Exception e) {
             System.err.print("testGetAllBook " + e.getMessage());
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public List<Offer> getAllOfferByBooks(String isbn) throws Exception {
+        ServerInterface serverInterface = new ServerInterface();
+        try {
+            String jsonBody = serverInterface.makeRequest(String.format(ServerConfiguration.DEFAULT_HOST_COMMERCIAL_OFFERS,isbn));
+            Log.i(TAG, "getAllOfferByBooks jsonBoby = " + jsonBody +"\n");
+
+            JSONObject offerts = new JSONObject(jsonBody);
+
+            Gson gson = new Gson();
+            Type collectionType = new TypeToken<List<Offer>>() {}.getType();
+            return gson.fromJson(offerts.getJSONArray("offers").toString(), collectionType);
+        } catch (Exception e) {
+            System.err.print("getAllOfferByABookCorrect " + e.getMessage());
             throw new Exception(e.getMessage());
         }
     }
