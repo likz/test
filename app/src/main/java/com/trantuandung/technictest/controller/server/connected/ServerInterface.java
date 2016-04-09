0ab1c054b9186919c15ac61fc18ec41f6133394c
TRAN Tuan Dung <t2dung8x@gmail.com>
2016-04-09 11:42:01 +0200

    feat(Server): add new method makeRequest

--- app/src/main/java/com/trantuandung/technictest/controller/server/connected/ServerInterface.java
+++ app/src/main/java/com/trantuandung/technictest/controller/server/connected/ServerInterface.java
@@ -1,4 +1,37 @@
 package com.trantuandung.technictest.controller.server.connected;
 
+import android.text.TextUtils;
+
+import com.trantuandung.technictest.exception.RequestErrorException;
+import com.trantuandung.technictest.exception.UrlEmptyErrorException;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class ServerInterface {
+
+    public String makeRequest(String url) throws IOException, UrlEmptyErrorException, RequestErrorException {
+        if(TextUtils.isEmpty(url)){
+            throw new UrlEmptyErrorException("url empty");
+        }
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        Response response = client.newCall(request).execute();
+        int responseCode = response.code();
+
+        if (responseCode >= 200 && responseCode < 300){
+            return response.body().string();
+        }else{
+            throw new RequestErrorException("Request error, response code = " + responseCode);
+        }
+    }
 }
