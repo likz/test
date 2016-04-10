6a4df3c09822067aac9ec62754c1c621225f4c0a
TRAN Tuan Dung <t2dung8x@gmail.com>
2016-04-10 20:56:53 +0200

    feat(Offer): integrate CommercialOfferCallBack and getOffert from DB and server

--- app/src/main/java/com/trantuandung/technictest/model/CommercialOffer.java
+++ app/src/main/java/com/trantuandung/technictest/model/CommercialOffer.java
@@ -1,4 +1,49 @@
 package com.trantuandung.technictest.model;
 
+import com.trantuandung.technictest.listener.CartListener;
+import com.trantuandung.technictest.listener.CommercialOfferCallBack;
+import com.trantuandung.technictest.server.ItemsRequester;
+
+import java.util.List;
+
 public class CommercialOffer {
+    CartListener cartListener;
+
+    public CommercialOffer(CartListener cartListener){
+        this.cartListener = cartListener;
+    }
+
+    public void getOffert(final CommercialOfferCallBack callback) {
+        final List<Book> books = cartListener.getBookList();
+        if (books.isEmpty()) {
+            callback.success(0);
+        }else{
+            ItemsRequester itemsRequester = new ItemsRequester();
+            try {
+                List<Offer> offerList = itemsRequester.getAllOfferByBooks(isbnValues(cartListener.getBookList()));
+                int bestOffer = 0;
+                for (Offer offer : offerList) {
+                    int currentOffer = offer.calculOffer(books);
+                    if (currentOffer > bestOffer) {
+                        bestOffer = currentOffer;
+                    }
+                }
+                callback.success(cartListener.totalCart() - bestOffer);
+            } catch (Exception e) {
+                callback.failure(e.getMessage());
+            }
+
+        }
+    }
+
+    private String isbnValues(List<Book> bookList) {
+        StringBuilder isbnValues = new StringBuilder();
+        for (Book book : bookList) {
+            if (isbnValues.length() != 0) {
+                isbnValues.append(",");
+            }
+            isbnValues.append(book.getIsbn());
+        }
+        return isbnValues.toString();
+    }
 }
