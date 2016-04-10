b2c9815987b80bdffec9a9843afce90414386dd3
TRAN Tuan Dung <t2dung8x@gmail.com>
2016-04-10 19:40:14 +0200

    feat(Toolbar): add news buttons cart, continue shopping and total

--- app/src/main/java/com/trantuandung/technictest/activity/MainActivity.java
+++ app/src/main/java/com/trantuandung/technictest/activity/MainActivity.java
@@ -1,11 +1,14 @@
 package com.trantuandung.technictest.activity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.trantuandung.technictest.R;
@@ -17,10 +20,12 @@ import com.trantuandung.technictest.enums.UserAdapterType;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private final static String TAG = MainActivity.class.getSimpleName();
     private UserAdapterType userAdapterType = UserAdapterType.NORMAL;
     DBHelper mDbHelper;
+    BooksAdapter bookAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,24 +42,34 @@ public class MainActivity extends AppCompatActivity {
         this.mDbHelper = new DBHelper(this);
     }
 
+
     @Override
     protected void onResume() {
         super.onResume();
-        initView();
 
+        TextView t = (TextView) findViewById(R.id.mainToolbarTotalSuggestedPrice);
+        t.setPaintFlags(t.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        findViewById(R.id.mainToolbarContinueShopping).setOnClickListener(this);
+        findViewById(R.id.mainToolbarCart).setOnClickListener(this);
+
+        initView();
     }
 
     private void initView() {
-        final RecyclerView contentListView = (RecyclerView) findViewById(R.id.main_content);
+        makeVisibilityButtonOnToolbar();
+        final RecyclerView contentListView = (RecyclerView) findViewById(R.id.mainContent);
         if (contentListView != null) {
             ItemsRequester itemsRequester = new ItemsRequester();
-            BooksAdapter bookAdapter = null;
+            List<Book> bookList;
             switch (userAdapterType){
                 case PANNIER:
+                    bookList = mDbHelper.getBookList();
+                    bookAdapter = new BooksAdapter(mDbHelper,bookList);
                     break;
                 default:
                     try {
-                        List<Book> bookList = itemsRequester.getAllBook();
+                        bookList = itemsRequester.getAllBook();
                         bookAdapter = new BooksAdapter(mDbHelper,bookList);
                     } catch (Exception e) {
                         Toast.makeText(this, getResources().getText(R.string.error_technical_problem_happened),Toast.LENGTH_SHORT).show();
@@ -67,5 +82,26 @@ public class MainActivity extends AppCompatActivity {
             contentListView.setAdapter(bookAdapter);
             contentListView.setHasFixedSize(true);
         }
+    }
+
+    private void makeVisibilityButtonOnToolbar(){
+        boolean isOnPannier = userAdapterType == UserAdapterType.PANNIER;
+        findViewById(R.id.mainToolbarCart).setVisibility(isOnPannier ? View.GONE : View.VISIBLE);
+        findViewById(R.id.mainToolbarTotal).setVisibility(isOnPannier ? View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.mainToolbarContinueShopping:
+                userAdapterType = UserAdapterType.NORMAL;
+                initView();
+                break;
+            case R.id.mainToolbarCart:
+                userAdapterType = UserAdapterType.PANNIER;
+                initView();
+                break;
+        }
+
     }
 }
