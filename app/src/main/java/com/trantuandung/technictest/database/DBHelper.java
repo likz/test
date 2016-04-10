00b9671000514f8c709b1fa06044835fe42b35b9
TRAN Tuan Dung <t2dung8x@gmail.com>
2016-04-10 17:09:05 +0200

    feat(DB): integrate BookListener and CartListener to DBHelper

--- app/src/main/java/com/trantuandung/technictest/database/DBHelper.java
+++ app/src/main/java/com/trantuandung/technictest/database/DBHelper.java
@@ -1,47 +1,41 @@
 package com.trantuandung.technictest.database;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import com.trantuandung.technictest.database.contract.BookContract;
-import com.trantuandung.technictest.database.contract.OfferContract;
+import com.trantuandung.technictest.listener.BookListener;
+import com.trantuandung.technictest.listener.CartListener;
+import com.trantuandung.technictest.model.Book;
 
-public class DBHelper extends SQLiteOpenHelper {
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHelper extends SQLiteOpenHelper implements CartListener,BookListener {
     private final String TAG = DBHelper.class.getName();
 
     // DB Name, same is used to name the sqlite DB file
-    private static final String DB_NAME = "dbTestTechnic";
+    private static final String DB_NAME = "dbtest";
     // If you change the database schema, you must increment the database version.
     private static final int DB_VERSION = 1;
 
-    //###############################################################
-    // TABLE LIST AND CREATION SCRIPT LIST
-    //###############################################################
-    // TABLE NAME LIST
-    private static final String[] TABLE_NAMES = new String[]{
-            BookContract.TABLE_NAME,
-            OfferContract.TABLE_NAME
-    };
-    //
-    // TABLE CREATION SCRIPT LIST
-    private static final String[] TABLE_SCRIPTS = new String[]{
-            BookContract.CREATE_TABLE,
-            OfferContract.CREATE_TABLE
-    };
-
     public DBHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
+        this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Called when the database is created for the
-        // first time. This is where the creation of
-        // tables and the initial population of the tables should happen.
-        createTables(db);
+        Log.i(TAG, "createTables");
+
+        try {
+            db.execSQL(BookContract.CREATE_TABLE);
+        } catch (SQLException e) {
+            Log.e(TAG, "createTables ERROR on: " + e.getMessage());
+        }
     }
 
     @Override
@@ -49,7 +43,6 @@ public class DBHelper extends SQLiteOpenHelper {
         Log.i(TAG, "onUpgrade: " + oldVersion + " > " + newVersion);
         if (oldVersion < newVersion) {
             Log.i(TAG, "onUpgrade reindex");
-//            DebugTracer.trace("onUpgrade");
             // ADD INDEXES
             reindex(db);
         }
@@ -59,61 +52,74 @@ public class DBHelper extends SQLiteOpenHelper {
         try {
             // INDEX DE LA TABLE DES FICHIERS
             db.execSQL(String.format("CREATE INDEX IF NOT EXIST %s_index on %s (%s)", BookContract.TABLE_NAME, BookContract.TABLE_NAME, BookContract.COL_ISBN));
-            db.execSQL(String.format("CREATE INDEX IF NOT EXIST %s_index on %s (%s, %s)", OfferContract.TABLE_NAME, OfferContract.TABLE_NAME, OfferContract.COL_TYPE, OfferContract.COL_VALUE));
         } catch (Exception e) {
             Log.e(TAG, "dropTables ERROR creating indexes on: " + e.getMessage());
         }
     }
 
-    public void createTables(SQLiteDatabase db) {
-        Log.i(TAG, "createTables");
-//        DebugTracer.trace("createTables");
-        for (String script : TABLE_SCRIPTS) {
-            try {
-                db.execSQL(script);
-            } catch (SQLException e) {
-                Log.e(TAG, "createTables ERROR on: " + e.getMessage());
-            }
-        }
-    }
-
-    public void dropTables(SQLiteDatabase db) {
-        Log.i(TAG, "dropTables");
-        for (String tablename : TABLE_NAMES) {
-            if (tablename.equalsIgnoreCase(BookContract.TABLE_NAME)) {
-                continue;
-            }
-            try {
-                db.execSQL("DROP TABLE IF EXISTS " + tablename);
-                Log.i(TAG, "dropTables: " + tablename + " dropped");
-            } catch (Exception e) {
-                Log.e(TAG, "dropTables ERROR: " + tablename + " does not exists");
-            }
-        }
-    }
-
-    /**
-     * Drop tables and recreate them.
-     * <p>
-     * Cleanup the tables just like
-     * </p>
-     */
-    public void recreateTables(SQLiteDatabase db) {
-        Log.i(TAG, "recreateTables");
-        if (db != null) {
+    @Override
+    public void addBook(Book book) {
+        SQLiteDatabase db = getWritableDatabase();
+        try {
             db.beginTransaction();
-            try {
-                dropTables(db);
-                createTables(db);
-                db.setTransactionSuccessful();
-                Log.i(TAG, "recreateTables | SQLiteDatabase transaction successful!");
-            } catch (Exception e) {
-                Log.e(TAG, "recreateTables | SQLiteDatabase transaction error: " + e.getMessage());
-            } finally {
-                db.endTransaction();
-            }
-        } else {
-            Log.e(TAG, "recreateTables | SQLiteDatabase is null");
+            Log.e(TAG, "addBook: " + book.getTitle());
+            db.execSQL("INSERT INTO book (isbn, title, price, cover) VALUES (?, ?, ?, ?)",
+                    new Object[]{book.getIsbn(), book.getTitle(), book.getPrice(), book.getCover()});
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            Log.e(TAG, "addBook ERROR on: " + e.getMessage());
+        } finally {
+            db.endTransaction();
         }
+    }
+
+    @Override
+    public void deleteBook(Book book) {
+        SQLiteDatabase db = getWritableDatabase();
+        try {
+            db.beginTransaction();
+            Log.e(TAG, "deleteBook: " + book.getTitle());
+            db.execSQL("DELETE FROM book WHERE book._id = ?",
+                    new Object[]{book.getId()});
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            Log.e(TAG, "deleteBook ERROR on: " + e.getMessage());
+        } finally {
+            db.endTransaction();
+        }
+
+    }
+
+    @Override
+    public int totalCart() {
+        int total;
+        Cursor cursor = getReadableDatabase().rawQuery("SELECT SUM(price) FROM book", null);
+        cursor.moveToFirst();
+        if (cursor.getCount() == 0) {
+            total = 0;
+        } else {
+            total = cursor.getInt(0);
+        }
+        cursor.close();
+        return total;
+    }
+
+    @Override
+    public List<Book> getBookList() {
+        List<Book> bookList = new ArrayList<>();
+        Cursor bookCursor = getReadableDatabase().rawQuery("SELECT * FROM book", null);
+        if (bookCursor != null && bookCursor.getCount() > 0) {
+            boolean hasNext = bookCursor.moveToFirst();
+            while (hasNext) {
+                bookList.add(new Book(bookCursor));
+                hasNext = bookCursor.moveToNext();
+            }
+        }
+
+        if (bookCursor != null) {
+            bookCursor.close();
+        }
+
+        return bookList;
     }
 }
